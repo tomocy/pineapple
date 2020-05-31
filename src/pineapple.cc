840ba4801bb7846fd06129198d7426f1e16eb39a
tomocy <tomocy.dev@gmail.com>
2020-05-31 16:32:59 +0900

    update Command
    Command prints help message if the action is not specified.

--- src/pineapple.cc
+++ src/pineapple.cc
@@ -2,15 +2,18 @@
 
 #include <functional>
 #include <iostream>
-#include <map>
 #include <string>
-#include <tuple>
-#include <vector>
 
 namespace pineapple {
+Command::Command(const std::string& name,
+                 const std::string& description) noexcept
+    : Command(name, description, [this]() { this->PrintHelp(); }) {}
+
 Command::Command(const std::string& name, const std::string& description,
                  const typename Command::action_t& action) noexcept
     : name(name), description(description), action(action) {}
+
+void Command::PrintHelp() const noexcept { std::cout << Help() << std::endl; }
 
 std::string Command::Help() const noexcept {
   return name + " - " + description;
--- src/pineapple.h
+++ src/pineapple.h
@@ -5,14 +5,16 @@
 #include <string>
 
 namespace pineapple {
-using command_action_t = std::function<void()>;
-
 class Command {
  public:
-  using action_t = command_action_t;
+  using action_t = std::function<void()>;
+
+  Command(const std::string& name, const std::string& description) noexcept;
 
   Command(const std::string& name, const std::string& description,
           const action_t& action) noexcept;
+
+  void PrintHelp() const noexcept;
 
   std::string Help() const noexcept;
 
