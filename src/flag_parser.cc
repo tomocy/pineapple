ec18788756a3a9498e73d439c61a3da3ee218339
tomocy <tomocy.dev@gmail.com>
2020-05-31 18:14:23 +0900

    add pineapple::TokenKind, Token

--- src/flag_parser.cc
+++ src/flag_parser.cc
@@ -1,6 +1,12 @@
 #include "src/flag_parser.h"
 
+#include <string>
 #include <vector>
+
+namespace pineapple {
+Token::Token(TokenKind kind, const std::string& literal) noexcept
+    : kind(kind), literal(literal) {}
+}  // namespace pineapple
 
 namespace pineapple {
 Lexer::Lexer(const std::vector<char>& src) noexcept : src(src) {}
--- src/flag_parser.h
+++ src/flag_parser.h
@@ -1,7 +1,23 @@
 #ifndef TOMOCY_FLAG_PARSER_H
 #define TOMOCY_FLAG_PARSER_H
 
+#include <string>
 #include <vector>
+
+namespace pineapple {
+enum class TokenKind {};
+}
+
+namespace pineapple {
+class Token {
+ public:
+  Token(TokenKind kind, const std::string& literal) noexcept;
+
+ private:
+  TokenKind kind;
+  std::string literal;
+};
+}  // namespace pineapple
 
 namespace pineapple {
 class Lexer {
