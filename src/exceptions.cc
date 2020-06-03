2bd144fe9d49d7c95d879a140a3d72802fbadfec
tomocy <tomocy.dev@gmail.com>
2020-06-03 12:03:35 +0900

    add exceptions bazel package

---
+++ src/exceptions.cc
@@ -0,0 +1,9 @@
+#include "src/exceptions.h"
+
+#include <string>
+
+namespace pineapple {
+Exception::Exception(const std::string& msg) noexcept : msg(msg) {}
+
+const std::string& Exception::What() const noexcept { return msg; }
+}  // namespace pineapple
---
+++ src/exceptions.h
@@ -0,0 +1,18 @@
+#ifndef TOMOCY_PINEAPPLE_EXCEPTIONS_H
+#define TOMOCY_PINEAPPLE_EXCEPTIONS_H
+
+#include <string>
+
+namespace pineapple {
+class Exception {
+ public:
+  explicit Exception(const std::string& msg) noexcept;
+
+  const std::string& What() const noexcept;
+
+ private:
+  std::string msg;
+};
+}  // namespace pineapple
+
+#endif
