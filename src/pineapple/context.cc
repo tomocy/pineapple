c0b8a3410fc6becf2cae94b5f30f76db63e489b7
tomocy <tomocy.dev@gmail.com>
2020-06-04 17:39:52 +0900

    update Context
    Context has the flag set.

--- src/pineapple/context.cc
+++ src/pineapple/context.cc
@@ -1,1 +1,7 @@
 #include "src/pineapple/context.h"
+
+#include "external/flags/src/flags/flags.h"
+
+namespace pineapple {
+Context::Context(flags::FlagSet&& flags) noexcept : flags(std::move(flags)) {}
+}
--- src/pineapple/context.h
+++ src/pineapple/context.h
@@ -1,8 +1,16 @@
 #ifndef TOMOCY_PINEAPPLE_PINEAPPLE_CONTEXT_H
 #define TOMOCY_PINEAPPLE_PINEAPPLE_CONTEXT_H
 
+#include "external/flags/src/flags/flags.h"
+
 namespace pineapple {
-class Context {};
+class Context {
+ public:
+  Context(flags::FlagSet&& flags) noexcept;
+
+ private:
+  flags::FlagSet flags;
+};
 }  // namespace pineapple
 
 #endif
