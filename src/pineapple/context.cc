35c53a2fcfa5dae4029becb7f6cc9313c66db151
tomocy <tomocy.dev@gmail.com>
2020-06-04 17:35:13 +0900

    add context bazel target

---
+++ src/pineapple/context.cc
@@ -0,0 +1,1 @@
+#include "src/pineapple/context.h"
---
+++ src/pineapple/context.h
@@ -0,0 +1,8 @@
+#ifndef TOMOCY_PINEAPPLE_PINEAPPLE_CONTEXT_H
+#define TOMOCY_PINEAPPLE_PINEAPPLE_CONTEXT_H
+
+namespace pineapple {
+class Context {};
+}  // namespace pineapple
+
+#endif
